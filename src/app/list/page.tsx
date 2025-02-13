51123a43e5bd5bffc5687f21ee5d0b1b0ba65c72
gitaehee <rainwifekth@naver.com>
2025-02-13 19:24:56 +0900

    feat: map 기초

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,13 +1,27 @@
+import { useEffect } from "react";
+
+
 export default function List() {
-  
+    let 상품 = ['Tomatoes', 'Pasta', 'Coconut'] // DB에서 가져온 데이터라고 치자
+    let 어레이 = [2,3,4]
+
+    useEffect(() => {
+        어레이.map(() => {
+            console.log('안녕'); // 이러면 어레이 수 많큼 안녕이 출력됨
+        });
+    }, []);
+
     return (
       <div>
         <h4 className="title">상품목록</h4>
         <div className="food">
-            <h4>상품1 $40</h4>
+            <h4>{상품[0]} $40</h4>
         </div>
         <div className="food">
-            <h4>상품2 $40</h4>
+            <h4>{상품[1]} $40</h4>
+        </div>
+        <div className="food">
+            <h4>{상품[2]} $40</h4>
         </div>
       </div>
     );
