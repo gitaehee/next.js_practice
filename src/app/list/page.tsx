6ce38f26c13bac25d7b3436f725567c786a73bdb
gitaehee <rainwifekth@naver.com>
2025-02-13 19:54:30 +0900

    feat: 강의4 완료

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,29 +1,18 @@
 export default function List() {
     let 상품 = ['Tomatoes', 'Pasta', 'Coconut'] // DB에서 가져온 데이터라고 치자
-    let 어레이 = [2,3,4]
-
-    어레이.map((a, i) => {
-        console.log(i); // log에 a 넣으면 2,3,4 나오고
-        // log에 i 넣으면 0,1,2 나옴
-    });
-
-    let b = 어레이.map((a)=>{
-        return 10 // return에 적은 걸 array로 담아줌 -> [10,10,10]
-    })
-    console.log(b)
-
+    
     return (
       <div>
         <h4 className="title">상품목록</h4>
-        <div className="food">
-            <h4>{상품[0]} $40</h4>
-        </div>
-        <div className="food">
-            <h4>{상품[1]} $40</h4>
-        </div>
-        <div className="food">
-            <h4>{상품[2]} $40</h4>
-        </div>
+        {
+            상품.map((a, i)=>{
+                return (
+                    <div className="food" key={i}> {/* key에 유니크한 값 */}
+                        <h4>{상품[i]} $40</h4> {/* i 넣어야 0부터 2까지 돌아감 */}
+                    </div>
+                )
+            })
+        }
       </div>
     );
   }
